6a7a052da71570a0761d85a8968fe04b5bf83bf1
herozqtc <317690952@qq.com>
2026-02-26 17:44:59 +0800

    Commit exercise 1.5
    finish the exercise one

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+ball_height = 100 # in meters
+bounce_factor = 0.6
+num_bounces = 0
+
+for i in range(10):
+    ball_height = ball_height * bounce_factor
+    num_bounces = num_bounces + 1
+    print('Bounce', num_bounces, 'Height', round(ball_height, 4))
