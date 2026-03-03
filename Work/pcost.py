cc223ac6db6891746cc553c48a8fc6493e968355
herozqtc <317690952@qq.com>
2026-03-03 16:13:50 +0800

    update exerciese file unit
    uodate to use file and params

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,16 +2,25 @@
 #
 # Exercise 1.27
 import os
+import sys
 import gzip
 
-with gzip.open('Work/Data/portfolio.csv.gz','rt') as f:
-    next(f) # skip header
-    sum = 0.0
-    for line in f:
-        name, shares, price = line.split(',')
-        shares = int(shares)
-        price = float(price)
-        cost = shares * price
-        sum = sum + cost
-    f.close()
-print('Total cost', round(sum, 2))
+def sumcount(filename):
+    with open(filename,'rt') as f:
+        next(f) # skip header
+        sum = 0.0
+        for line in f:
+            name, shares, price = line.split(',')
+            shares = int(shares)
+            price = float(price)
+            cost = shares * price
+            sum = sum + cost
+        f.close()
+    print('Total cost', round(sum, 2))
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+    if os.path.isfile(filename):
+        sumcount(filename)
+    else:
+        print('File not found:', filename)
