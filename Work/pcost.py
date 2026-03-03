e6d84b9fbcd8bd1ec69169ef1898037e53fe5cc8
herozqtc <317690952@qq.com>
2026-03-03 15:53:13 +0800

    exercise three
    the file handled exercise

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+import os
+import gzip
+
+with gzip.open('Work/Data/portfolio.csv.gz','rt') as f:
+    next(f) # skip header
+    sum = 0.0
+    for line in f:
+        name, shares, price = line.split(',')
+        shares = int(shares)
+        price = float(price)
+        cost = shares * price
+        sum = sum + cost
+    f.close()
+print('Total cost', round(sum, 2))
